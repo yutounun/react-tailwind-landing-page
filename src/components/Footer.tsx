375d6830617f233c4cf26d74940179626dd35705
yutounun <geek.yuto@gmail.com>
2024-12-22 09:31:04 -0800

    footer desktop

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,18 @@ import Logo from "./Logo";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col gap-6 bg-[#F4F6F9] px-[30px] py-16 text-neutral-100 bg-primary">
-      <Logo />
+    <div className="flex flex-col md:flex-row md:justify-around gap-6 bg-[#F4F6F9] px-[30px] py-16 text-neutral-100 bg-primary">
+      <div className="flex flex-col w-full md:w-64">
+        <Logo />
 
-      <p className="text-sm">
-        ShipUp delivers an unparalleled customer service through dedicated
-        customer teams, engaged people working in an agile culture, and a global
-        footprint
-      </p>
+        <p className="text-sm">
+          ShipUp delivers an unparalleled customer service through dedicated
+          customer teams, engaged people working in an agile culture, and a
+          global footprint
+        </p>
+      </div>
 
-      <div className="flex gap-10">
+      <div className="flex gap-10 md:gap-60">
         <div className="flex flex-col gap-4">
           <span className="font-bold">Explore</span>
           <div className="flex flex-col gap-2 text-sm">
